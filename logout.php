5d0e5dda303ba8b8c26521e17ed20176809d2407
PJ0707 <83937855+PJ0707@users.noreply.github.com>
2021-07-18 12:23:55 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -14,7 +14,7 @@ session_start();
 <body>
 	<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK8fuVbbuh10ytUiy1rvITnnNrdM0FOWKTqeSL1VednQDkNqAznoz_Kyyz-y3nM5Tw9w&usqp=CAU" width="100%" height="100%">
 	<div class="c">
-		<a href="main.php"><button class="button"><span id="font1"><b>GoBack>></b> </span></button></a>
+		<a href="index.php"><button class="button"><span id="font1"><b>GoBack>></b> </span></button></a>
 </div>
 </body>
 </html>
